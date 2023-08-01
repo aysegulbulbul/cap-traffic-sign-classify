aaf99cdb639ccaed35f7e2a2ffc39cae26ff687c
Ayşegül <azmlmhnds@gmail.com>
2023-08-01 18:28:16 +0300

    RENAME:file named capsule changed to TrafficSignClassify

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
 import logging.config
 import yaml
 
-with open('capsules/capsule/src/configs/logging_config.yaml', 'r') as f:
+with open('capsules/TrafficSignClassify/src/configs/logging_config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
     logging.captureWarnings(True)
