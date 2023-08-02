7dd3b726ec5156dad3fbb26880382e6b2b644ce5
Ayşegül <azmlmhnds@gmail.com>
2023-08-02 09:11:44 +0300

    RENAME: capsules name for imports

--- apps/inference.py
+++ apps/inference.py
@@ -20,10 +20,12 @@ ENDPOINT_URL = "http://127.0.0.1:8000/api"
 #web tarafına ihtiyaç duymadan test etmek = Çıkarım dosyası
 def inference():
     config = Config.from_json(CFG)
-    image_data =Image(name="image", uID="323332", mime_type="image/png", encoding="base64",value =image.encode64(np.asarray(cv2.imread(config.project.path +'/capsules/capsule/resources/00000.png')).astype(np.float32),'image/png'), type="imageList", field="img")
+    image_data =Image(name="image", uID="323332", mime_type="image/png", encoding="base64",value =image.encode64(np.asarray(cv2.imread(config.project.path +'/capsules/TrafficSignClassify/resources/00000.png')).astype(np.float32),'image/png'), type="imageList", field="img")
+
     traffic = configTypeTraffic(value="traffic")
     configTypevalue = ConfigType(value=traffic)
     trafficConfigs = TrafficConfigs(configType=configTypevalue, name="Configs")#
+
     imageList = ImageList(name="ImageList", value=[image_data], type="imageList", field="img")
     inputImage = InputImage(value=imageList)
     trafficInputs = TrafficInputs(inputImage=inputImage, name="Inputs", value="Inputs")
@@ -38,6 +40,6 @@ def inference():
     #print(request_json)
 
 
-
 if __name__ =="__main__":
+    print("ksdfjkdsj")
     inference()
